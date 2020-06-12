c8ad1b6918d1eeb16ed270773751311f13fd7058
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2020-06-12 12:33:38 -0500

    Adding Comments

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,3 +1,7 @@
+/**
+ * This function request JSON data to /data endpoint.
+ * JSON data is displayed in the DOM.
+ */
 async function getData() {
     const response = await fetch("/data");
     const quote = await response.text();
