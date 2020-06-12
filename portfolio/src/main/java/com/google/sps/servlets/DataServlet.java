b16da82ff19373779c18468c5419e82c53b1a90f
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2020-06-12 12:13:10 -0500

    Post Method

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -58,4 +58,24 @@ public class DataServlet extends HttpServlet {
     response.setContentType("application/json;");
     response.getWriter().println(json);
   }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // Get the input from the form.
+    String text = getParameter(request, "text-input", "");
+    text = text.toUpperCase();
+    System.out.println(text);
+  }
+
+  /**
+   * @return the request parameter, or the default value if the parameter was not
+   *         specified by the client
+   */
+  private String getParameter(HttpServletRequest request, String name, String defaultValue) {
+    String value = request.getParameter(name);
+    if (value == null) {
+      return defaultValue;
+    }
+    return value;
+  }
 }
